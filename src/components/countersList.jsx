1954c821ec4ec0b1e672289c09ab2ecb28112d5e
BuildTools <unconfigured@null.spigotmc.org>
2022-11-19 20:04:00 +0600

    add files

---
+++ src/components/countersList.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import Counter from "./counter";
+
+const CountersList = () => {
+  const initialState = [
+    { id: 0, value: 0, name: "Ненужная вещь" },
+    { id: 1, value: 0, name: "Ложка" },
+    { id: 2, value: 0, name: "Вилка" },
+    { id: 3, value: 0, name: "Тарелка" },
+    { id: 4, value: 0, name: "Набор минималиста" },
+  ];
+
+  const [counters, setCounters] = useState(initialState);
+
+  const handleDelete = (id) => {
+    const newCounters = counters.filter((c) => c.id !== id);
+    setCounters(newCounters);
+  };
+
+  const handleReset = () => {
+    setCounters(initialState);
+  };
+
+  const handleIncrement = (id) => {
+    const newValues = counters.map((counter) => {
+      if (counter.id === id) {
+        return { ...counter, value: counter.value + 1 };
+      }
+      return counter;
+    });
+    setCounters(newValues);
+  };
+
+  const handleDecrement = (id) => {
+    const newValues = counters.map((counter) => {
+      if (counter.id === id) {
+        return { ...counter, value: counter.value - 1 };
+      }
+      return counter;
+    });
+    setCounters(newValues);
+  };
+
+  return (
+    <>
+      {counters.map((count) => (
+        <Counter
+          key={count.id}
+          onDelete={handleDelete}
+          onIncrement={handleIncrement}
+          onDecrement={handleDecrement}
+          {...count}
+        />
+      ))}
+      <button className="btn btn-primary btn-sm m-2" onClick={handleReset}>
+        Сброс
+      </button>
+    </>
+  );
+};
+export default CountersList;
